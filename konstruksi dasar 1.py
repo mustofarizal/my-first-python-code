13a2ff95768c6fee8f8a1c0f1853cee586cbbd94
Rizal Mustofa <mustofarizal1994@gmail.com>
2022-01-28 23:17:54 +0700

    sekuensial dan percabangan

---
+++ konstruksi dasar 1.py
@@ -0,0 +1,21 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('by Rizal')
+print('Tanggal 27 Januari 2022')
+print('-' * 10)
+
+# PERCABANGAN: Eksekusi terpilih
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('jalan lain')
+
+# PERULANGAN
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak+1): #jumlah perulangan = 5 - 1 = 4
+    print(f'Halo anak #{index_anak}')
+
+
