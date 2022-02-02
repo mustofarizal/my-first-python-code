98215fa5ddd012a85bb2085cf40c14fe3fabc50e
Rizal Mustofa <mustofarizal1994@gmail.com>
2022-02-02 21:16:45 +0700

    tipe data list

---
+++ dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,53 @@
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First','4DX']
+print('tampilkan variable daftar_buku')
+print(daftar_buku)
+
+print('\nproses semua dengan in')
+for buku in daftar_buku:
+    print(buku)
+
+print('\ntampilkan isi daftar_buku pada indeks tertentu')
+print(daftar_buku[0])
+print(daftar_buku[2])
+
+print('\nTampilkan dengan for in range')
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku = [1,'Kenji Volume 2', -313, 3.14]
+print('\nTampilkan dengan for in range, dimana tipe data tiap elemen berbeda2')
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('Kembalikan nilai awal daftar_buku')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First','4DX']
+print('\nTambahkan 1 buku baru')
+daftar_buku.append ('Dunia Matematik Kelas 5')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nClear list')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen pertama')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First','4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop -1')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Things First','4DX']
+daftar_buku.pop(-4)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
